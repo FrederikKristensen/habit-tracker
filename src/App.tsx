f0e8c72933be39a1e51d9c83386e10293d8b4916
Frederik Kristensen <frede4947@gmail.com>
2026-06-05 23:19:52 +0200

    Basic setup of styling

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,18 @@ function App() {
 
   return (
     <>
-      <div>Habits</div>
+      <div className="text-white font-bold bg-mist-800 max-w-2xs mx-auto p-3 m-8  rounded-md">
+        <button>Habits</button>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-        <button type="submit">Add</button>
-      </form>
+      <div className="text-white font-semibold bg-mist-700 max-w-xs mx-auto p-2">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+          <button type="submit">Add</button>
+        </form>
+      </div>
 
-      <div>
+      <div className="text-white bg-mist-600 max-w-sm mx-auto p-0.5 m-3">
         {habits.map((habit, index) => (
           <Habitcard key={index} name={habit} />
         ))}
--- src/components/Habitcard.tsx
+++ src/components/Habitcard.tsx
@@ -12,10 +12,10 @@ const Habitcard = ({ name }: HabitcardProps) => {
   };
 
   return (
-    <div>
-      <button onClick={toggleCheck}>
-        {isChecked ? 'y' : 'O'}
-      </button>
+    <div className="flex items-center m-2 bg-mist-400">
+      <div className="mr-2 border-3 border-black size-9">
+        <button onClick={toggleCheck}>{isChecked ? 'y' : 'O'}</button>
+      </div>
       <div>{name}</div>
     </div>
   );
