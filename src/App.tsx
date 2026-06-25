b795553aaa75596f06f0a9efb6221f65c46e62ce
Frederik Kristensen <frede4947@gmail.com>
2026-06-25 19:23:22 +0200

    Set up of react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,16 @@
-import React, { useState } from 'react';
-import Habitcard from './components/Habitcard';
-import './css/App.css';
+import { Route, Routes } from 'react-router-dom';
+import Todos from './pages/todos';
+import Habits from './pages/habits';
 
-export interface Habit {
-  id: string;
-  name: string;
-  selectedDays: Set<string>;
-}
-
-function App() {
-  const [habits, setHabits] = useState<Habit[]>([]);
-  const [input, setInput] = useState('');
-
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (input.trim()) {
-      setHabits([
-        ...habits,
-        {
-          id: Date.now().toString(),
-          name: input,
-          selectedDays: new Set(),
-        },
-      ]);
-      setInput('');
-    }
-  };
-
-  const toggleDay = (habitId: string, day: string) => {
-    setHabits(
-      habits.map((habit) => {
-        if (habit.id === habitId) {
-          const newSelectedDays = new Set(habit.selectedDays);
-          if (newSelectedDays.has(day)) {
-            newSelectedDays.delete(day);
-          } else {
-            newSelectedDays.add(day);
-          }
-          return { ...habit, selectedDays: newSelectedDays };
-        }
-        return habit;
-      })
-    );
-  };
-
+const App = () => {
   return (
     <>
-      <div className="max-w-2xs mx-auto text-white font-bold bg-mist-800  p-3 m-8  rounded-md">
-        <button>Habits</button>
-      </div>
-
-      <div className="max-w-xs mx-auto p-3 bg-mist-700 rounded-lg">
-        <form onSubmit={handleSubmit} className="flex">
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Add a habit..."
-            className="flex-1 text-white focus:outline-none"
-          />
-          <button type="submit">Add</button>
-        </form>
-      </div>
-
-      <div className="max-w-sm mx-auto p-0.5 m-3 bg-mist-600 text-white rounded-sm">
-        {habits.map((habit) => (
-          <Habitcard key={habit.id} habit={habit} onToggleDay={toggleDay} />
-        ))}
-      </div>
+      <Routes>
+        <Route path="/" element={<Todos />} />
+        <Route path="/habits" element={<Habits />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
