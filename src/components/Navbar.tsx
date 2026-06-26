3fc7646526d4956c5ebcd2d7191f45b096a20e46
Frederik Kristensen <frede4947@gmail.com>
2026-06-26 16:08:49 +0200

    Current page in navbar is brighter color
    Made it so todos and habits in the navbar becomes gray if they are not the active page. This should make it alot easier to see which page is current

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const navStyle = ({ isActive }: { isActive: boolean }) =>
+    `flex-1 text-center font-bold ${isActive ? 'text-white' : 'text-gray-400'}`;
+
   return (
-    <nav className="max-w-2xs mx-auto flex gap-2 m-7 font-bold text-white bg-mist-800 p-3 rounded-md">
-      <NavLink to="/todos" className="flex-1 text-center">Todos</NavLink>
-      <NavLink to="/habits" className="flex-1 text-center">Habits</NavLink>
+    <nav className="max-w-2xs mx-auto flex gap-2 m-7 bg-mist-800 p-3 rounded-md">
+      <NavLink to="/todos" className={navStyle}>
+        Todos
+      </NavLink>
+      <NavLink to="/habits" className={navStyle}>
+        Habits
+      </NavLink>
     </nav>
   );
 };
